b7fb476b40178a238149aeeab86e3cc87ea276fd
PeTroll7 <60701752+PeTroll7@users.noreply.github.com>
2023-11-07 11:31:09 +0100

    Update socket-io.service.ts

--- src/app/services/socket-io.service.ts
+++ src/app/services/socket-io.service.ts
@@ -13,7 +13,7 @@ export class SocketIoService {
   constructor() {}
 
   connect() {
-    this.socket = io('safari-bar.up.railway.app');   //https://safri-bar-server.herokuapp.com
+    this.socket = io('https://safari-bar-server.onrender.com');   //https://safri-bar-server.herokuapp.com
   }
 
   connectToRoom(gameID: string) {
